46d0510d930ae3afc2cf04aacde26859308d11e1
jibranharis <jibranharis@users.noreply.github.com>
2026-03-23 21:22:11 -0700

    Fix ERR_TOO_MANY_REDIRECTS

--- next.config.ts
+++ next.config.ts
@@ -4,15 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  async redirects() {
-    return [
-      {
-        source: '/Opportunities',
-        destination: '/opportunities',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
